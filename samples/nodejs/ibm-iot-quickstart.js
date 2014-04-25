c7fa4be4b742702a7a530c749ef7034ace1c7cf1
Stuart Hayton <sjhx@users.noreply.github.com>
2014-04-25 13:27:54 +0100

    Ensure deviceId is lower case

--- samples/nodejs/ibm-iot-quickstart.js
+++ samples/nodejs/ibm-iot-quickstart.js
@@ -1,3 +1,18 @@
+//*****************************************************************************
+// Copyright (c) 2014 IBM Corporation and other Contributors.
+//
+// All rights reserved. This program and the accompanying materials
+// are made available under the terms of the Eclipse Public License v1.0
+// which accompanies this distribution, and is available at
+// http://www.eclipse.org/legal/epl-v10.html 
+//
+// Contributors:
+//  IBM - Initial Contribution
+//*****************************************************************************
+
+// IoT Cloud QuickStart Driver
+// A sample IBM Internet of Things Cloud Quickstart Service client for Intel Galileo
+
 var mqtt = require('mqtt');
 var fs = require('fs');
 
@@ -10,8 +25,8 @@ var client;
 
 require('getmac').getMac(function(err, macAddress) {
   if (err) throw err;
-  deviceId = macAddress.replace(/:/g, '');
   console.log("MAC Address: " + macAddress);
+  deviceId = macAddress.replace(/:/g, '').toLowerCase();
   client = mqtt.createClient(port,broker, { "clientId": "quickstart:"+deviceId } );
   topic = "iot-1/d/" + deviceId + "/evt/galileo-quickstart/json"; 
   console.log("Topic: " + topic);
@@ -23,7 +38,6 @@ setInterval(sendMessage,1000);
 function sendMessage() {
 	var message = {};
 	message.d = {};
-	//message.d.date = Date.now();
 	//read the CPU temp from sysfs
 	fs.readFile('/sys/class/thermal/thermal_zone0/temp','utf8',function (err, data) {
 		if (err) throw err;
@@ -31,7 +45,5 @@ function sendMessage() {
 		console.log(message);
 		client.publish(topic,JSON.stringify(message));
 	});
-	
-
 }
 
