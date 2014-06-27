1d223d473a03e18cc53bd4fa8a417a8ff453f5e3
Tim-Daniel Jacobi <tim.jacobi@uk.ibm.com>
2014-06-27 16:40:08 +0100

    update code

--- samples/nodejs/ibm-iot-quickstart.js
+++ samples/nodejs/ibm-iot-quickstart.js
@@ -15,7 +15,7 @@
 
 var mqtt = require('mqtt');
 var fs = require('fs');
-var configFile = "./device.sample.cfg";
+var configFile = "./device.cfg";
 
 
 var port = 1883;
@@ -25,42 +25,85 @@ var client;
 
 require('getmac').getMac(function(err, macAddress) {
     if (err) throw err;
+    macAddress.toString().replace(/:/g, '').toLowerCase();
+
     require('properties').parse(configFile, { path: true }, function (err, config){
         var options = {};
         
         // Set the configuration used when no device.cfg is present
         var organization = "quickstart";
-        var deviceType = "galileo-quickstart";
+        var deviceType = "iotsample-galileo";
 
         // If device.cfg was loaded successfully update the configuarion
         if(config){
-            organization = config.org || organization;
-            deviceType = config.type || deviceType;
-            macAddress = config.id || macAddress;
+
+            if(!config.org){
+                throw "Configuration should include an org field that specifies your organization.";
+            }
+
+            if(!config.type){
+                throw "Configuration should include a type field that specifies your device type.";
+            }
+
+            if(!config.id){
+                throw "Configuration should include an id field that specifies your device id.";
+            }
+
+            organization = config.org;
+            deviceType = config.type;
+            macAddress = config.id;
 
             broker = organization + ".messaging.internetofthings.ibmcloud.com";
 
             if(config.token){
-                options.username = organization;
+                options.username = organization; // Actual value of options.username can be set to any string
                 options.password = config.token;
             }
         }
         
-        deviceId = macAddress.toString().replace(/:/g, '').toLowerCase();
-        options.clientId = "d:" + organization + ":" + deviceType + ":" + deviceId;
+        options.clientId = "d:" + organization + ":" + deviceType + ":" + macAddress;
         client = mqtt.createClient(port, broker, options);
-        topic = "iot-2/evt/" + deviceType + "/fmt/json"; 
+        topic = "iot-2/evt/status/fmt/json";
+
+        var interval = setInterval(sendMessage,1000);
+
+        if(config){
+            client.subscribe('/iot-2/cmd/+/fmt/json');
+
+            client.on('message', function(topic, message) {
+                console.log('Received command on topic: ' + topic);
+                
+                var msg;
+                try {
+                    msg = JSON.parse(message);
+                }
+                catch (e) {
+                    msg = {};
+                    console.log("Couldn't parse recieved command. Please ensure it is valid JSON.");
+                }
+
+                if(msg.hasOwnProperty('send-status')){
+                    if(msg['send-status']){
+                        if(!interval){
+                            interval = setInterval(sendMessage,1000);
+                        }
+                    }
+                    else {
+                        clearInterval(interval);
+                        interval = false;
+                    }
+                }
+            });
+        }
           
         
         console.log("Broker: " + broker);
-        console.log("MAC Address: " + macAddress);
+        console.log("Device ID: " + macAddress);
         console.log("Topic: " + topic);
-        console.log("Connection options: ")
+        console.log("Connection options: ");
         console.log(options);
     });
 });
-
-setInterval(sendMessage,1000);
 
 function sendMessage() {
     var message = {};
