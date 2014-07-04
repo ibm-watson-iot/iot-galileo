93e71561926fb23a07a5e676d23bef3ae6bd6d02
Tim-Daniel Jacobi <tim.jacobi@uk.ibm.com>
2014-07-04 14:22:21 +0100

    IOT V2

--- samples/nodejs/ibm-iot-quickstart.js
+++ samples/nodejs/ibm-iot-quickstart.js
@@ -15,35 +15,117 @@
 
 var mqtt = require('mqtt');
 var fs = require('fs');
+var configFile = "./device.cfg";
 
 
 var port = 1883;
-var broker = '184.172.124.189';
+var broker = "quickstart.messaging.internetofthings.ibmcloud.com";
 var topic;
 var client;
 
-
 require('getmac').getMac(function(err, macAddress) {
-  if (err) throw err;
-  console.log("MAC Address: " + macAddress);
-  deviceId = macAddress.replace(/:/g, '').toLowerCase();
-  client = mqtt.createClient(port,broker, { "clientId": "quickstart:"+deviceId } );
-  topic = "iot-1/d/" + deviceId + "/evt/galileo-quickstart/json"; 
-  console.log("Topic: " + topic);
+    if (err) throw err;
+    
+    macAddress = macAddress.toString().replace(/:/g, '').toLowerCase();
+
+    require('properties').parse(configFile, { path: true }, function (err, config){
+        var options = {};
+        
+        // Set the configuration used when no device.cfg is present
+        var organization = "quickstart";
+        var deviceType = "iotsample-galileo";
+
+        // If device.cfg was loaded successfully update the configuarion
+        if(config){
+
+            if(config['auth-method']){
+                if(config['auth-method'] !== "token"){
+                    throw "Authentication method not supported. Please make sure you use \"token\".";
+                }
+                if(config['auth-method'] && !config['auth-token']){
+                    throw "Authentication method set to \"token\" but no \"auth-token\" setting was provided in device.cfg";
+                }
+
+                options.username = "use-token-auth"; // Actual value of options.username can be set to any string
+                options.password = config['auth-token'];
+            }
+
+            if(!config.org){
+                throw "Configuration should include an org field that specifies your organization.";
+            }
+
+            if(!config.type){
+                throw "Configuration should include a type field that specifies your device type.";
+            }
+
+            if(!config.id){
+                throw "Configuration should include an id field that specifies your device id.";
+            }
+
+            console.log("Configuration loaded successfully, connecting your device to the registered service.");
+
+            organization = config.org;
+            deviceType = config.type;
+            macAddress = config.id;
+
+            broker = organization + ".messaging.internetofthings.ibmcloud.com";
+        }
+        else {
+            console.log("No configuration file found, connecting to the quickstart servcice.");
+        }
+        
+        options.clientId = "d:" + organization + ":" + deviceType + ":" + macAddress;
+        client = mqtt.createClient(port, broker, options);
+        topic = "iot-2/evt/status/fmt/json";
+
+        var interval = setInterval(sendMessage,1000);
+
+        if(config){
+            client.subscribe('/iot-2/cmd/+/fmt/json');
+
+            client.on('message', function(topic, message) {
+                console.log('Received command on topic: ' + topic);
+                
+                var msg;
+                try {
+                    msg = JSON.parse(message);
+                }
+                catch (e) {
+                    msg = {};
+                    console.log("Couldn't parse recieved command. Please ensure it is valid JSON.");
+                }
+
+                if(msg.hasOwnProperty('send-status')){
+                    if(msg['send-status']){
+                        if(!interval){
+                            interval = setInterval(sendMessage,1000);
+                        }
+                    }
+                    else {
+                        clearInterval(interval);
+                        interval = false;
+                    }
+                }
+            });
+        }
+          
+        
+        console.log("Broker: " + broker);
+        console.log("Device ID: " + macAddress);
+        console.log("Topic: " + topic);
+        console.log("Connection options: ");
+        console.log(options);
+    });
 });
 
-
-setInterval(sendMessage,1000);
-
 function sendMessage() {
-	var message = {};
-	message.d = {};
-	//read the CPU temp from sysfs
-	fs.readFile('/sys/class/thermal/thermal_zone0/temp','utf8',function (err, data) {
-		if (err) throw err;
-		message.d.cputemp = data/1000;
-		console.log(message);
-		client.publish(topic,JSON.stringify(message));
-	});
+    var message = {};
+    message.d = {};
+    //read the CPU temp from sysfs
+    fs.readFile('/sys/class/thermal/thermal_zone0/temp','utf8', function (err, data) {
+        if (err) throw err;
+        message.d.cputemp = data/1000;
+        console.log(message);
+        client.publish(topic, JSON.stringify(message));
+    });
 }
-
